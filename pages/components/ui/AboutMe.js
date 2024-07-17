4c970dfa8c25a1a1ef48d918ea3beb5eb9a3cfee
Ansh Patel <pate1011@umn.edu>
2024-07-16 21:29:38 -0500

    an about me :)

--- pages/components/ui/AboutMe.js
+++ pages/components/ui/AboutMe.js
@@ -40,10 +40,12 @@ const AboutMe = () => {
                 />
             </Box>
             <Text fontSize="xl" fontWeight="bold">Ansh Patel</Text>
-            <Text fontSize="md" textAlign="center" px={3}>
-                Passionate Software Engineer | Tech Innovator | Student
+            <Text fontSize="md" textAlign="justify" justifyContent="space-between" px={8} py={2}>
+                Hi there 👋 I'm Ansh Patel, a Computer Science student at the University of Minnesota with a passion for software engineering.
+                As an aspiring developer, I'm always seeking out new challenges to build upon my skills, often through hackathons and personal projects.
+                When I'm not coding, I enjoy playing and watching sports, traveling to new places, and exploring different cultures.
             </Text>
-            <Button mt={5} colorScheme="yellow" onClick={handleConnectClick}>Connect with Me</Button>
+            <Button mt={2} colorScheme="yellow" onClick={handleConnectClick}>Connect with Me</Button>
         </Box>
     );
 };
